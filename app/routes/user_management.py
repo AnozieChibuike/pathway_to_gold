119e56fe14abf1a6859bd195130ae17fbc412d82
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-04-03 11:41:53 +0100

    newpuh

--- app/routes/user_management.py
+++ app/routes/user_management.py
@@ -58,6 +58,14 @@ def user():
             message = f"Could not create user, missing required parameter: {e}"
             data = {"message": message, "data": {}, "status": "error"}
             return jsonify(data), 406
+    if request.method == "PUT":
+        try:
+            body = request.json
+            return update_user(body)
+        except KeyError as e:
+            message = f"Could not update user, missing required parameter: {e}"
+            data = {"message": message, "data": {}, "status": "error"}
+            return jsonify(data), 406
     args = request.args
     email = args.get("email")
     user_id = args.get("id")
@@ -72,6 +80,7 @@ def user():
         user = Users.get_or_404(username=username)
         return jsonify({"data": user.to_dict()}), 200
     return jsonify(None), 200
+
 
 def create_user(body):
     fullname = body["fullname"]
@@ -115,9 +124,58 @@ def create_user(body):
     user.set_password(password)
     user.save()
     email_body = f"Here is your otp: {otp} Expires in 10 minutes"
-    message, code, status = send_mail('Verify Email', email, body=email_body)
+    message, code, status = send_mail("Verify Email", email, body=email_body)
     if not status:
-        data = {"message": message, "user": user.to_dict(), "status": "pending"}, code    
+        data = {"message": message, "user": user.to_dict(), "status": "pending"}, code
     else:
         data = {"message": message, "user": user.to_dict(), "status": "success"}, code
     return jsonify(data), 201
+
+
+def update_user(body):
+    user_id = body["id"]
+    user = Users.get_or_404(id=user_id)
+    if "fullname" in body:
+        user.fullname = body["fullname"]
+    # if "email" in body:
+        user_exist = Users.get(email=body["email"])
+        if user_exist:
+            message = f"User exists with supplied email"
+            data = {
+                "message": message,
+                "data": {},
+                "status": "error",
+                "reason": "email",
+            }
+            return jsonify(data), 406
+        user.email = body["email"]
+    if "password" in body:
+        user.set_password(body["password"])
+    if "username" in body:
+        user_exist = Users.get(username=body["username"])
+        if user_exist:
+            message = f"User exists with supplied username"
+            data = {
+                "message": message,
+                "data": {},
+                "status": "error",
+                "reason": "username",
+            }
+            return jsonify(data), 406
+        user.username = body["username"]
+    # if "phone" in body:
+        user_exist = Users.get(phone=body["phone"])
+        if user_exist:
+            message = f"User exists with supplied Phone number"
+            data = {
+                "message": message,
+                "data": {},
+                "status": "error",
+                "reason": "phone",
+            }
+            return jsonify(data), 406
+        user.phone = body["phone"]
+    if "pin" in body:
+        user.pin = body["pin"]
+    user.save()
+    return jsonify({"data": user.to_dict()}), 201
