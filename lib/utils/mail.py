d021be9a02ad5ca4ce767cfaa64d0b6c0904bdcc
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-04-07 14:14:06 +0100

    mail

--- lib/utils/mail.py
+++ lib/utils/mail.py
@@ -1,7 +1,8 @@
 from flask_mail import Message
 from app import mail, app
 
-DEFAULT_SENDER = app.config.get("MAIL_DEFAULT_SENDER")
+DEFAULT_SENDER_MAIL = app.config.get("MAIL_DEFAULT_SENDER")
+DEFAULT_SENDER_NAME = app.config.get("MAIL_DEFAULT_SENDER_NAME")
 
 
 def send_mail(
@@ -9,13 +10,13 @@ def send_mail(
     recipient: str,
     html: str = None,
     body: str = None,
-    sender: str = 'noreply@pathway2gold.com',
+    sender: tuple = (DEFAULT_SENDER_NAME,DEFAULT_SENDER_MAIL),
 ):
     try:
         msg = Message(subject, recipients=[recipient])
         msg.body = body
         msg.html = html
-        msg.sender = ("P2G",sender)
+        msg.sender = sender
         mail.send(msg)
         return "Email sent", 200, True
     except Exception as e:
